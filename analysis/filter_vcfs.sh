a8c554504dbcacd599b3de00ee8fc1b250bb1687
Ubuntu <ubuntu@ip-10-41-20-30.ec2.internal>
2022-09-06 22:19:57 +0000

    update nf1 analyses

--- analysis/filter_vcfs.sh
+++ analysis/filter_vcfs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+## STRELKA2
 ## Uses the files from `synapse get -q "SELECT * FROM syn31864875.1" `
 ## Filters out mutations not labeled as PASS by Strelka2 and mutations with a quality score <20
 
@@ -8,11 +9,11 @@ do
     bcftools filter $i -i 'FILTER="PASS" && %QUAL>20 && CHR=="chr17" && POS>31094927 && POS<31382116' -o $i.NF1.vcf.gz
 done
 
-for i in *.filtered.vcf.gz
-do
-    bcftools sort $i -o $i
-    bcftools index $i
-done
+# for i in *.filtered.vcf.gz
+# do
+#     bcftools sort $i -o $i
+#     bcftools index $i
+# done
 
 ##V1 had a messed up source files, I had to re-retrieve them, so the latest versions of the output files on synapse are the correct ones
 
@@ -24,4 +25,27 @@ done
 for i in *.NF1.vcf.gz
 do
     synapse store $i --parentId syn33016669
+done
+
+## DEEPVARIANT
+## Uses the files from `synapse get -q "SELECT * FROM syn31546989.1"`
+## Filters out mutations not labeled as PASS by Strelka2 and mutations with a quality score <20
+
+for i in *.vcf.gz;
+do
+    ic=${i%_VEP.ann.vcf.gz} 
+    
+    echo $ic
+    bcftools filter $i -i 'FILTER="PASS" && %QUAL>20' -o DeepVariant_$ic.filtered.vcf.gz 
+done
+
+# for i in DeepVariant_*.filtered.vcf.gz
+# do
+#     bcftools sort $i -o $i
+#     bcftools index $i
+# done
+
+for i in DeepVariant_*.filtered.vcf.gz
+do
+    synapse store $i --parentId syn35591895
 done
